69389f40b6587035d6ca70ed8649852fddfb9b1a
fixed400 <fixedz534@gmail.com>
2017-10-03 14:32:26 +0300

    refactor

--- app/src/main/java/notexpad/zenolbs/com/notexpad/DB.java
+++ app/src/main/java/notexpad/zenolbs/com/notexpad/DB.java
@@ -8,27 +8,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-/*
------------------------------------------------------------------------------------
-Простой базы данных заметок помощник доступа класса. Определяет основные операции CRUD
-  * Для примера блокнота, и дает возможность перечислить все заметки, а также
-  * Восстановить или изменить определенный сведению.
-  *
-  * Это была улучшена от первой версии этого руководства по
-  * добавление обработки ошибок лучше, а также с помощью возвращения курсора, а не
-  * использования коллекцию внутренних классов (которая является менее масштабируемым и не
-  рекомендуется *).
-  * /
-
-/ * Это не оригинальный код, который блокнот упражнения предусмотрено.
-  *
-  * Я сделал некоторые редактирования на этот код, добавив параметр даты на нем.
-от Атора
-  * /
-
-
- */
-
 public class DB {
 
     public static final String FIELD_ROWID = "_id";
@@ -38,22 +17,11 @@ public class DB {
 
 
     private static final String L_TAG = "DATABASE";
-    private SelfDBHelper selfDbHelper; //класс, являющийся наследником для SQLiteOpenHelper
-
-  /*
-   * Для управления базой данных SQLite существует класс SQLiteDatabase.
-   * В классе SQLiteDatabase определены методы query(), insert(), delete() и update() для чтения,
-   *  добавления, удаления, изменения данных.
-   *  Кроме того, метод execSQL() позволяет выполнять любой допустимый код на языке SQL
-   */
+    private SelfDBHelper selfDbHelper;
 
 
-    private SQLiteDatabase mDb; //SQLiteDatabase.(stock class) Содержит методы для работы с данными – т.е. вставка, обновление, удаление и чтение.
+    private SQLiteDatabase mDb;
 
-    /**
-     * Database creation sql statement
-     База данных SQL заявление создание
-     */
     private static final String DATABASE_CREATE =
             "create table notes (_id integer primary key autoincrement, "
                     + "title text not null, body text not null, date text not null);";
@@ -63,9 +31,9 @@ public class DB {
     private static final int DATABASE_VERSION = 1;
 
     private final Context a_context;
-    //====================================== inside class ===============================
+
     private static class SelfDBHelper extends SQLiteOpenHelper {
-        //конструктор
+
         SelfDBHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
@@ -84,30 +52,21 @@ public class DB {
             onCreate(db);
         }
     }
-//=====================================================================================
 
     public DB(Context context) {
         this.a_context = context;
     }
-
-
-
-    //throws - бросает  аналог исключения try{}cath{}final  -- обработка исключения вызывающим методом
     public DB open() throws SQLException {
 
-        // инициализация конструктором
+
         selfDbHelper = new SelfDBHelper(a_context); //private DatabaseHelper mDbHelper; //класс, являющийся наследником для SQLiteOpenHelper
-        //Метод getWritableDatabase выполняет подключение к базе данных и возвращает нам объект SQLiteDatabase для работы с ней.
-        mDb = selfDbHelper.getWritableDatabase(); // private DatabaseHelper mDbHelper; //класс, являющийся наследником для SQLiteOpenHelper
+        mDb = selfDbHelper.getWritableDatabase();
         return this;
     }
 
     public void close() {
         selfDbHelper.close();
     }
-
-
-    //----------------------------------------------------------------------------------
 
     public long createNote(String title, String body, String date) {
         ContentValues initialValues = new ContentValues();
@@ -126,35 +85,14 @@ public class DB {
 
 
     public Cursor getAllContents() {
-    	/*
-    	 * distinct -	true if you want each row to be unique, false otherwise.
-           table	 - The table name to compile the query against.
 
-    	columns – список полей, которые мы хотим получить
-
-    	selection – строка условия WHERE
-    	selectionArgs – массив аргументов для selection. В selection можно использовать знаки ? , а которые будут заменены этими значениями.
-    	groupBy - группировка
-    	having – использование условий для агрегатных функций
-    	orderBy - сортировка
-
-    	*/
-        //distinct,table,columns ,selection ,selectionArgs,  groupBy, having, orderBy
-
-        //String orderBy = null;
-        //String orderBy = "DESC";
-        //String orderBy = "ASC";
-        //String orderBy = "KEY_ROWID = DESC";
         String orderBy = FIELD_ROWID + " DESC";
-
-
         return mDb.query(DATABASE_TABLE,
                 new String[] {FIELD_ROWID, FIELD_TITLE, FIELD_BODY,FIELD_DATE},
                 null, null, null, null,
                 orderBy);
     }
 
-//--------------------------------------------------------------------------------
     public Cursor fetchByName(String inputText) throws SQLException {
         Log.w(L_TAG, inputText);
         Cursor mCursor = null;
@@ -175,10 +113,7 @@ public class DB {
         return mCursor;
     }
 
-
     public Cursor getOne(long rowId) throws SQLException {
-
-
 
         Cursor _idCursor = mDb.query(true,
                 DATABASE_TABLE,
@@ -192,19 +127,14 @@ public class DB {
 
     }
 
-    //----------------------------------------------------------------------------------
 
     public boolean updateNote(long rowId, String title, String body,String date) {
         ContentValues args = new ContentValues();
         args.put(FIELD_TITLE, title);
         args.put(FIELD_BODY, body);
 
-        //This lines is added for personal reason
-        //Этой линии добавляется по личным причинам
         args.put(FIELD_DATE, date);
 
-        //One more parameter is added for data
-        //Еще один параметр добавляется для данных
         return mDb.update(DATABASE_TABLE, args, FIELD_ROWID + "=" + rowId, null) > 0;
     }
 
--- app/src/main/java/notexpad/zenolbs/com/notexpad/settings/PreferencesActivity.java
+++ app/src/main/java/notexpad/zenolbs/com/notexpad/settings/PreferencesActivity.java
@@ -4,7 +4,6 @@ package notexpad.zenolbs.com.notexpad.settings;
  * Created by grd on 8/5/17.
  */
 
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
@@ -18,7 +17,6 @@ import notexpad.zenolbs.com.notexpad.R;
 
 import static notexpad.zenolbs.com.notexpad.NoteEditor.mBodyText;
 import static notexpad.zenolbs.com.notexpad.NoteEditor.mTitleText;
-//import static notexpad.zenolbs.com.notexpad.NoteEditor.mDateText;
 
 public class PreferencesActivity extends PreferenceActivity {
 
@@ -33,16 +31,6 @@ public class PreferencesActivity extends PreferenceActivity {
     public static String listChooserItem2;
 
     public static boolean isBigText;
-
-
-    private int mNoteNumber = 1;
-    public Context context;
-
-
-    final String CURSIVE ="cursive";
-    final String SANS_SERIF_LIGHT ="sans-serif-light";
-    final String MONOSPACE ="monospace";
-
 
     private static int typeface ;
     private static String style ;
@@ -87,34 +75,11 @@ public class PreferencesActivity extends PreferenceActivity {
 
     static final String SAVED_TEXT = "saved_text";
 
-    /*
-    int typeface = Typeface.NORMAL;
-    int typeface = Typeface.BOLD;
-    int typeface = Typeface.ITALIC;
-    int typeface = Typeface.BOLD_ITALIC;
-    */
-
-    /*
-    float size = 10.f;
-    float size = 14.f;
-    float size = 18.f;
-    float size = 24.f;
-    */
-
-
-
-
-
-   //EditText editText = new NoteEditor().mBodyText;
-
-
         @Override
         public void onCreate(final Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
-
-          //  sharedPreferences = getPreferences(MODE_PRIVATE);
 
             sharedPreferences = PreferenceManager
                     .getDefaultSharedPreferences(this);
@@ -123,55 +88,8 @@ public class PreferencesActivity extends PreferenceActivity {
 
         }
 
-
-            // https://stackoverflow.com/questions/14343903/what-is-the-equivalent-of-androidfontfamily-sans-serif-light-in-java-code
-
-        /*
-        These are the list of default font family used, use any of this by replacing the double quotation string "sans-serif-medium"
-
-        FONT FAMILY                    TTF FILE
-
-        1  casual                      ComingSoon.ttf
-        2  cursive                     DancingScript-Regular.ttf
-        3  monospace                   DroidSansMono.ttf
-        4  sans-serif                  Roboto-Regular.ttf
-        5  sans-serif-black            Roboto-Black.ttf
-        6  sans-serif-condensed        RobotoCondensed-Regular.ttf
-        7  sans-serif-condensed-light  RobotoCondensed-Light.ttf
-        8  sans-serif-light            Roboto-Light.ttf
-        9  sans-serif-medium           Roboto-Medium.ttf
-        10  sans-serif-smallcaps       CarroisGothicSC-Regular.ttf
-        11  sans-serif-thin            Roboto-Thin.ttf
-        12  serif                      NotoSerif-Regular.ttf
-        13  serif-monospace            CutiveMono.ttf
-
-
-            editText.setTextColor(Color.BLUE);
-            // https://stackoverflow.com/questions/3203694/custom-fonts-in-android
-            // mBodyText.setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
-            // mBodyText.setTypeface(Typeface.create("sans-serif-light", Typeface.BOLD));
-            // mBodyText.setTypeface(Typeface.create("sans-serif-light", Typeface.ITALIC));
-            // mBodyText.setTypeface(Typeface.create("sans-serif-light", Typeface.BOLD_ITALIC));
-            editText.setTypeface(Typeface.create("monospace", Typeface.NORMAL));
-            editText.setTypeface(Typeface.create("cursive", Typeface.NORMAL));
-
-            editText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 55.f);
-            //mBodyText.setHeight(54);
-            */
-
-
-    //============================
-    // try  it https://developer.android.com/training/basics/data-storage/shared-preferences.html?hl=ru
-    // - http://www.fandroid.info/sharedpreferences-sohranenie-dannyh-v-postoyannoe-hranilishhe-android/
-    // try -  MODE_MULTI_PROCESS — несколько процессов совместно используют один файл SharedPreferences.
-    //   Caused by: java.lang.NullPointerException:
-    // Attempt to invoke interface method 'android.content.SharedPreferences$Editor android.content.SharedPreferences.edit()' on a null object reference
-    // ВНИМАНИЕ! Все модификаторы кроме MODE_PRIVATE в настоящий момент объявлены deprecated и не рекомендуются к использованию в целях безопасности.
    public static void saveStatePass(boolean state) {
-       // need init context -  "java.lang.String android.content.Context.getPackageName()"
 
-       //SharedPreferences.Editor editor = sharedPreferences.edit();
-        //Editor editor = sharedPreferences.edit();
          editor = sharedPreferences.edit();
         editor.putBoolean(SAVED_TEXT,  state);
         editor.commit();
@@ -182,27 +100,24 @@ public class PreferencesActivity extends PreferenceActivity {
 
        boolean state;
        return state  = sharedPreferences.getBoolean(SAVED_TEXT,false);
-        //etText.setText(savedText);
 
     }
-    //============================
-
 
 
     public static void setTextBold(SharedPreferences sharedPreferences ){
-        //boolean getBoolean(String key, boolean defValue);
+
         boolean my_checkbox_preference = sharedPreferences.getBoolean("textThickness", false);
 
         textThick=my_checkbox_preference;
 
         if(textThick){
-            //new NoteEditor().mBodyText.setTypeface(Typeface.create("monospace", Typeface.BOLD));
+
             mBodyText.setTypeface(Typeface.create(getStyle(), Typeface.BOLD));
             mTitleText.setTypeface(Typeface.create(getStyle(), Typeface.BOLD));
 
             setTypeface(1);
         }else{
-            //new NoteEditor().mBodyText.setTypeface(Typeface.create("sans-serif-light", Typeface.NORMAL));
+
             mBodyText.setTypeface(Typeface.create(getStyle(), Typeface.NORMAL));
             mTitleText.setTypeface(Typeface.create(getStyle(), Typeface.NORMAL));
             setTypeface(0);
@@ -213,14 +128,14 @@ public class PreferencesActivity extends PreferenceActivity {
 
     public static void loadStyleText(SharedPreferences sharedPreferences) {
 
-        //loadPref();
+
         String listValue = sharedPreferences.getString("textFontPref", "не выбрано");
 
         listChooserItem =listValue;
 
         switch (listValue){
             case "1" :
-                /// animId =  R.anim.scaling_increase;
+
                 setStyle(1);
                 mBodyText.setTypeface(Typeface.create(getStyle(), getTypeface()));
                 mTitleText.setTypeface(Typeface.create(getStyle(), getTypeface()));
@@ -228,19 +143,19 @@ public class PreferencesActivity extends PreferenceActivity {
 
                 break;
             case "2":
-                // animId =  R.anim.scaling_protrusion;
+
                 setStyle(2);
                 mBodyText.setTypeface(Typeface.create(getStyle(), getTypeface()));
                 mTitleText.setTypeface(Typeface.create(getStyle(), getTypeface()));
                 break;
             case "3":
-                // animId =  R.anim.scaling_descent;
+
                 setStyle(3);
                 mBodyText.setTypeface(Typeface.create(getStyle(), getTypeface()));
                 mTitleText.setTypeface(Typeface.create(getStyle(), getTypeface()));
                 break;
             case "4":
-                // animId =  R.anim.scaling_descent;
+
                 setStyle(4);
                 mBodyText.setTypeface(Typeface.create(getStyle(), getTypeface()));
                 mTitleText.setTypeface(Typeface.create(getStyle(), getTypeface()));
@@ -251,21 +166,19 @@ public class PreferencesActivity extends PreferenceActivity {
     }
     public static void loadSizeText(SharedPreferences sharedPreferences) {
 
-        //loadPref();
         String listValue = sharedPreferences.getString("textSizePref", "не выбрано");
-
         listChooserItem2 =listValue;
 
         switch (listValue){
             case "1" :
-                // animId =  R.anim.scaling_increase;
+
                 mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10.f);
                 mTitleText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10.f);
                 isBigText=false;
 
                 break;
             case "2":
-                // animId =  R.anim.scaling_protrusion;
+
                 mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14.f);
                 mTitleText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14.f);
                 isBigText=false;
@@ -277,19 +190,19 @@ public class PreferencesActivity extends PreferenceActivity {
                 isBigText=false;
                 break;
             case "4":
-                // animId =  R.anim.scaling_descent;
+
                 mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24.f);
                 mTitleText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24.f);
                 isBigText=true;
                 break;
             case "5":
-                // animId =  R.anim.scaling_descent;
+
                 mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 32.f);
                 mTitleText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24.f);
                 isBigText=true;
                 break;
             case "6":
-                // animId =  R.anim.scaling_descent;
+
                 mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 48.f);
                 mTitleText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24.f);
                 isBigText=true;
@@ -308,27 +221,18 @@ public class PreferencesActivity extends PreferenceActivity {
 
         switch (listValue){
             case "1" :
-                // animId =  R.anim.scaling_increase;
-               // mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10.f);
                 mBodyText.setTextColor(Color.BLACK);
                 mTitleText.setTextColor(Color.BLACK);
-
                 break;
             case "2":
-                // animId =  R.anim.scaling_protrusion;
-               // mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18.f);
                 mBodyText.setTextColor(Color.RED);
                 mTitleText.setTextColor(Color.RED);
                 break;
             case "3":
-                // animId =  R.anim.scaling_descent;
-               // mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 24.f);
                 mBodyText.setTextColor(Color.parseColor("#aa29ae38"));
                 mTitleText.setTextColor(Color.parseColor("#aa29ae38"));
                 break;
             case "4":
-                // animId =  R.anim.scaling_descent;
-                //mBodyText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 32.f);
                 mBodyText.setTextColor(Color.BLUE);
                 mTitleText.setTextColor(Color.BLUE);
                 break;
@@ -337,7 +241,6 @@ public class PreferencesActivity extends PreferenceActivity {
 
     }
 
-    ///============================= Settings ===============================
     @Override
     protected void onDestroy() {
         super.onDestroy();

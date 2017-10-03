2b9decbae2959acd418e3455ba5695746242ca7b
fixed400 <fixedz534@gmail.com>
2017-10-03 14:54:27 +0300

    refactor pref

--- app/src/main/java/notexpad/zenolbs/com/notexpad/util/LockActivity.java
+++ app/src/main/java/notexpad/zenolbs/com/notexpad/util/LockActivity.java
@@ -19,37 +19,20 @@ import notexpad.zenolbs.com.notexpad.R;
 
 public class LockActivity extends Activity implements View.OnClickListener {
 
-    SharedPreferences sPref;
-
-    final String SAVED_TEXT = "saved_pass_notice";
     private static String TAG = "LockActivity_log";
+    final String SAVED_TEXT = "saved_pass_notice";
 
     EditText etText;
     TextView textView;
     Button btnSave, btnReset;
     CheckBox checkBox;
 
-
     boolean usePass, newPassAccept;
 
-
-    boolean appearance;
-    Boolean display;
-
-
-    static final String SAVED_TEXT_2 = "saved_pass_notice";
-
-
-    //static SharedPreferences sharedPreferences;
     private SharedPreferences sharedPreferences;
-
-    //public static SharedPreferences.Editor editor;
     private   SharedPreferences.Editor editor;
 
     public static String passLiteral;
-    //private  String passLiteral;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,18 +46,9 @@ public class LockActivity extends Activity implements View.OnClickListener {
         btnSave = (Button) findViewById(R.id.button_accept);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
 
-
         sharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(getApplicationContext());
         editor = sharedPreferences.edit();
-         /*
-        sPref = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor ed = sPref.edit();
-        ed.putString(SAVED_TEXT, etText.getText().toString());
-        ed.commit();
-        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
-        */
-
         checkBox.setOnClickListener(this);
 
     }
@@ -96,23 +70,15 @@ public class LockActivity extends Activity implements View.OnClickListener {
             textView.setText("off password");
             etText.setVisibility(View.INVISIBLE);
             btnSave.setEnabled(false);
-
         }
-
     }
-
-
 
     void saveStatePass(boolean state) {
 
         editor = sharedPreferences.edit();
         editor.putBoolean("my_pakage",  state);
         editor.commit();
-        //or
-        //editor.apply();
     }
-
-
 
     public  boolean loadStatePass() {
 
@@ -133,13 +99,9 @@ public class LockActivity extends Activity implements View.OnClickListener {
         if(state==null)
             state = false;
         return state ;
-        //==============================================
 
     }
 
-
-
-    // анализируем, какая кнопка была нажата. Всего один метод для всех кнопок
     @Override
     public void onClick(View view){
         usePass = checkBox.isChecked();
@@ -150,7 +112,7 @@ public class LockActivity extends Activity implements View.OnClickListener {
                         textView.setText("on password");
                         etText.setVisibility(View.VISIBLE);
                         btnSave.setEnabled(true);
-                       // saveStatePass(true); //- задействовать старые пароль (прошлый)
+
                         Toast.makeText(this, "Пароль обезопасит ваши данные", Toast.LENGTH_SHORT).show();
                     }else {
                         textView.setText("off password");
@@ -167,31 +129,12 @@ public class LockActivity extends Activity implements View.OnClickListener {
 
     public void btnAccept(View view) {
 
-        // попытка  отфильтровать символы
-        String a =etText.getText().toString();
         String str =etText.getText().toString();
-
-      //  if (a.trim().replaceAll("\\s+", " ").equalsIgnoreCase(b.trim().replaceAll("\\s+", " "))) {
-            // Strings equivalent
-      //  }
-        str.replaceAll(" ", "");// не работатет
-
-       // String str = a;
-       //  str.replaceAll(" ", "");
-         //str.replaceAll(" ", "");
-        // str.replaceAll("(?U)[\\pP\\s]", "");
-        str.toLowerCase(); // не работатет -   разобратся
-
-        String b = str;
-
         str.trim();
 
        if(str.equals("")||str.contains(".")||str.contains(",")||str.equals("^\\s+") ){
-
-
            if(str.contains(".")||str.contains(",")||str.contains("  ")||str.contains(" ")) {
                Toast.makeText(this, "Not aviable sign!", Toast.LENGTH_SHORT).show();
-
 
            }else {
                Toast.makeText(this, "Field must be not empty!", Toast.LENGTH_SHORT).show();
@@ -202,46 +145,33 @@ public class LockActivity extends Activity implements View.OnClickListener {
             saveText();
             newPassAccept =true;
             alertWarning();
-
         }
     }
-
-
 
     void saveText() {
 
         Toast.makeText(this, "Pass was saved", Toast.LENGTH_SHORT).show();
-        //--------------
         SharedPreferences sharedPreferences = PreferenceManager
                 .getDefaultSharedPreferences(getApplicationContext());
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(SAVED_TEXT, etText.getText().toString());
-        //editor.apply(); // новый способ
         editor.commit();
         saveStatePass(true);
     }
-
-
+    
    public  void loadText(Context context) {
 
        SharedPreferences sharedPreferences = PreferenceManager
                .getDefaultSharedPreferences(context);
-       //sharedPreferences  = getPreferences(MODE_PRIVATE);
        passLiteral = sharedPreferences.getString(SAVED_TEXT,"");
-       //----------------
        Toast.makeText(context, "Text was loaded", Toast.LENGTH_SHORT).show();
     }
 
-
-    /*
-    // !!! НЕ УДАЛЯТЬ (ЭТО ВТОРОЙ ВАРИАНТ ОБРАБОКИ СОХРАНЕНИЯ ПАРОЛЯ МЕСТО ЯВНОЙ КНОКИ)
     @Override
     public void onBackPressed() {
-
         if(etText.getText().toString().equals("")) {
             finish();
         }else {
-
             new AlertDialog.Builder(this)
                     .setTitle("Сохранить Ваш пароль ?")
                     .setMessage(etText.getText().toString())
@@ -258,19 +188,15 @@ public class LockActivity extends Activity implements View.OnClickListener {
         }
 
     }
-    */
+
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         passWarning();
-
-
     }
 
     private void passWarning() {
-     // установка опции реагирования перед выходом на последующее отображение пароля
         if(!newPassAccept){
 
             if(loadStatePass()){
@@ -278,10 +204,8 @@ public class LockActivity extends Activity implements View.OnClickListener {
             }else{
                 saveStatePass(false);
             }
-
             Toast.makeText(this, "!Pass Warning - New password not saved!", Toast.LENGTH_SHORT).show();
         }
-
 
     }
 
@@ -296,10 +220,5 @@ public class LockActivity extends Activity implements View.OnClickListener {
                     }
                 })
                .show();
-
-
     }
-
-
-
 }
